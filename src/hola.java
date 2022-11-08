6099d743e1b0bb268f99a80128bbc4b7b51bcce8
jose <jose.pinos@epn.edu.ec>
2022-11-08 11:08:41 -0500

    Creacion de J3 y asignacion de sus datos

--- src/hola.java
+++ src/hola.java
@@ -62,6 +62,13 @@ public class hola {
         System.out.println("Rapidez: " + J2.getRapidez());
         System.out.println("Defensa: " + J2.getDefensa());
 
+        Jugador J3 = new Jugador();
+        J3.setNombre("melvin eugene");
+        J3.setFuerza((short)12);
+        J3.setPrecision((short)12);
+        J3.setResistencia((short)12);
+        J3.setRapidez((short)12);
+        J3.setDefensa((short)12);
     }//fin de main
 
 }//fin de la clase hola
